629610079e2193a9d3cf43968124c03a6e52e124
nehao7 <157192328+nehao7@users.noreply.github.com>
2025-06-17 10:40:20 +0530

    Intent , Button click , Toast , findviewbyId

--- app/src/main/java/com/o7/androidkotlin6week25/MainActivity.kt
+++ app/src/main/java/com/o7/androidkotlin6week25/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.o7.androidkotlin6week25
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var btn:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+       btn=findViewById(R.id.btnNext)
+        btn?.setOnClickListener {
+            Toast.makeText(this,"On Click Message displayed",Toast.LENGTH_SHORT).show()
+            var intent=Intent(this,LinaerLayoutScreen::class.java)
+            startActivity(intent)
+        }
+
     }
 }
