36d525daa33caba58be92df2066237af8d6c7913
nehao7 <157192328+nehao7@users.noreply.github.com>
2025-06-19 17:02:20 +0530

    Relative Layout

--- app/src/main/java/com/o7/androidkotlin6week25/RelativeLayoutScreen.kt
+++ app/src/main/java/com/o7/androidkotlin6week25/RelativeLayoutScreen.kt
@@ -1,32 +1,20 @@
 package com.o7.androidkotlin6week25
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    var btn: Button?=null
+class RelativeLayoutScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_relative_layout_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        btn=findViewById(R.id.btnNext)
-
-        btn?.setOnClickListener {
-            Toast.makeText(this,"On Click Message displayed",Toast.LENGTH_SHORT).show()
-            var intent=Intent(this,LinaerLayoutScreen::class.java)
-            startActivity(intent)
-        }
-
     }
 }
